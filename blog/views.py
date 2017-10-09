f104bbe113e435574968daa7798c92810c66ab27
xingkongdadao <xingkongdadao@gmail.com>
2017-10-09 22:44:51 +0700

    添加index.html

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('欢迎光临')
+    return render(request, 'blog/index.html', context={
+        'title': '我的博客首页',
+        'welcome': '欢迎访问我的博客首页'
+    })
